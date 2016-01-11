dc81b206f87c494fab39c3a97ca8ff7b686c78bd
酒枫子 <maple_leaf_7@msn.com>
2016-01-11 17:02:42 +0800

    加参数

--- www/imageCropper.js
+++ www/imageCropper.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
-exports.crop = function(arg0, arg1, success, error) {
-    exec(success, error, "imageCropper", "crop", [arg0, arg1]);
+exports.crop = function(arg0, arg1, arg2,success, error) {
+    exec(success, error, "imageCropper", "crop", [arg0, arg1, arg2]);
 };
